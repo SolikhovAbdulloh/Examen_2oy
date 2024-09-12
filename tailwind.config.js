f21f103959d8c6211bc0038513f0a52a6f0d8ee4
SolihovAbdulloh <abdullasolikh@mail.com>
2024-09-12 09:38:09 +0500

    header finish responsive

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./html*/*.{html,js}"],
+  theme: {
+    colors: {
+      red: "#fff",
+      sariq: "#FF9619",
+      yashil: "#0B7572",
+      qora: "#363738",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
